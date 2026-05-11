7305b3aa6f89a3a20ced261ec1236320eb38627e
cultelise <cultelise@gmail.com>
2026-05-11 12:30:06 -0500

    interaction handling

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+	function goalInputHandler(enteredText) {
+		console.log(enteredText);
+	}
+
+	function addGoalHandler() {}
 	return (
 		<View style={styles.appContainer}>
 			<View style={styles.inputContainer}>
-				<TextInput placeholder='your course goal' style={styles.textInput} />
-				<Button title='add goal' />
+				<TextInput
+					placeholder='your course goal'
+					style={styles.textInput}
+					onChangeText={goalInputHandler}
+				/>
+				<Button title='add goal' onPress={addGoalHandler} />
 			</View>
 			<View style={styles.listContainer}>
 				<Text>List of goals...</Text>
@@ -16,13 +25,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
 	appContainer: {
+		flex: 1,
 		padding: 50,
+		paddingHorizontal: 16,
 	},
 	inputContainer: {
+		flex: 1,
 		flexDirection: 'row',
 		justifyContent: 'space-evenly',
+		alignItems: 'center',
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: 'black',
 	},
 	listContainer: {
+		flex: 5,
 		justifyContent: 'center',
 	},
 	textInput: {
